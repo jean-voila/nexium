4b5682ca8825b9c731f787797e6bb683f1bbc143
Milo Delbos <milo.delbos@epita.fr>
2025-02-14 18:27:14 +0100

    Values in file already, we deleted most of the code not needed anymore, it should work, if Antonin can try it

--- src/lib/sha256/main.rs
+++ src/lib/sha256/main.rs
@@ -48,8 +48,6 @@ check(https://blog.boot.dev/cryptography/how-sha-2-works-step-by-step-sha-256/) 
 
         Pseudo-Code from wikipedia for an example: https://en.wikipedia.org/wiki/SHA-2
  */
-use num_primes::Generator;
-use num_traits::ToPrimitive;
 
 //tranform a string in a binary string
 pub fn preprocessing(s: String) -> String {
@@ -74,27 +72,22 @@ pub fn preprocessing(s: String) -> String {
 }
 // function for processing, that's where we do all the job
 pub fn processing(s: String) {
-    let mut hash_val = [0u32; 8];
-    let mut round_const = [0u32; 64];
-    let mut primes = Vec::new();
-    while primes.len() < 64 {
-        let prime = Generator::new_prime(8);
-        if let Some(prime_u64) = prime.to_u64() {
-            if !primes.contains(&prime_u64) {
-                primes.push(prime_u64);
-            }
-        }
-    }
-    for i in 0..64 {
-        let sqrt_prime = (primes[i] as f64).sqrt();
-        let fractionnalpart = sqrt_prime - sqrt_prime.floor();
-        if i < 8 {
-            hash_val[i] = (fractionnalpart * (1u64 << 32) as f64) as u32;
-            round_const[i] = (fractionnalpart * (1u64 << 32) as f64) as u32;
-        } else {
-            round_const[i] = (fractionnalpart * (1u64 << 32) as f64) as u32;
-        }
-    }
+    let mut hash_val = [
+        0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab,
+        0x5be0cd19,
+    ];
+    let round_const = [
+        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4,
+        0xab1c5ed5, 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe,
+        0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f,
+        0x4a7484aa, 0x5cb0a9dc, 0x76f988da, 0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+        0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc,
+        0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 0xa2bfe8a1, 0xa81a664b,
+        0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070, 0x19a4c116,
+        0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+        0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7,
+        0xc67178f2,
+    ];
     let len = (s.len() * 8) as u64;
     let nbchunks = len / 512;
     let mut dataarray = [0u32; 64];
