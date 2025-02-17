2df08fcb4c86d713a6f70ef7aecbf6bad36543f2
Jean Herail <jean.herail@epita.fr>
2025-02-17 16:59:04 +0100

    replaced the base with a simple slash in the astro config

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nexium-jean-herail-ff3bbec6f6f2d53c3b619fbb2bb7f3a70234886c751d.pages.epita.fr/',
-  base: '/web',
+  base: '/',
   outDir: 'public',
   publicDir: 'static',
   integrations: [
