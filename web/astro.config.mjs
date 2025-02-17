cb3f95316f044f4635525b0dc8f959a4f9b496a2
Jean Herail <jean.herail@epita.fr>
2025-02-17 16:27:25 +0100

    modified the building process of the gitlab pages in gitlab-ci.yml

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jean.herail.gitlab.cri.epita.fr',
-  base: '/web',
+  base: '/nexium',
   outDir: 'public',
   publicDir: 'static',
   integrations: [
