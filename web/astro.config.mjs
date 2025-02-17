073dff44ab7c1b73eca4460e3f0da429075982f2
Jean Herail <jean.herail@epita.fr>
2025-02-17 16:54:44 +0100

    changed the base root of astro's build

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -4,8 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jean.herail.gitlab.cri.epita.fr',
-  base: '/nexium',
+  site: 'https://nexium-jean-herail-ff3bbec6f6f2d53c3b619fbb2bb7f3a70234886c751d.pages.epita.fr/',
+  base: '/web',
   outDir: 'public',
   publicDir: 'static',
   integrations: [
